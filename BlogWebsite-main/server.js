61343d2c5dbe153339074db4f64691a6820e15ca
anil0136 <anilmaikap0136@gmail.com>
2024-07-09 23:10:06 +0530

    blog

---
+++ BlogWebsite-main/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const articlesRouter = require("./routes/articles");
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const app = express();
+const dotenv = require('dotenv')
+const Article=require('./models/article')
+
+const username = process.env.MONGODB_USERNAME
+const password = process.env.MONGODB_PASSOWRD
+
+mongoose.connect(`mongodb+srv://${username}:${password}@charliee01.qqyy2ji.mongodb.net/blogWebsiteDB`)
+
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:false}));
+app.use(methodOverride('_method'))
+
+
+app.get("/", async (req, res) => {
+const articles = await Article.find().sort({createdAt:'desc'});
+  res.render("articles/index", { articles: articles });
+
+});
+
+app.use("/articles", articlesRouter);
+
+app.listen(3000);
